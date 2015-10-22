a4dd12227e317e16306315a697f4640ce4da9943
Wyatt Elliott <wyattelliott27@gmail.com>
2015-10-22 22:18:27 +0000

    just reset database

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,8 @@ class Picture < ActiveRecord::Base
     
      belongs_to :user
     
-     has_attached_file :image, styles: { medium: "500x500>" }
-         
-     validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+        has_attached_file :image, styles: { medium: "500x500>" }
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
     
 
