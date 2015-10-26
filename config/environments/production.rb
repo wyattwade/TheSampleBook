2b8c35fd91351f85ed5a225017aa5189f5555a93
Wyatt Elliott <wyattelliott27@gmail.com>
2015-10-26 23:07:04 +0000

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,18 @@ config.action_mailer.default_url_options = { :host => 'letsdothiss.herokuapp.com
 
 
 
+
+
+
+
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
+
+
+
+
+
+
 end
