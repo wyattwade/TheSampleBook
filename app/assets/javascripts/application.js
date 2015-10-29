b306b610e06d3311503e54c790760c2f5bd9d3aa
Wyatt Elliott <wyattelliott27@gmail.com>
2015-10-29 07:02:43 +0000

    took out turbolinks from app js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require masonry/jquery.masonry
 //= require_tree .
-//= require turbolinks
+
